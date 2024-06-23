b1800ea86050eabbfb9747768558f9abe3a7edec
Krystle Capuano <krystlecapuano@gmail.com>
2024-06-23 00:12:10 -0400

    Home Image Edit

--- Capuano_Krystle_Memorial_Capstone/src/components/HomeTron.jsx
+++ Capuano_Krystle_Memorial_Capstone/src/components/HomeTron.jsx
@@ -2,9 +2,15 @@ const HomeTron = () => {
   return (
 
 <div className="home-card">
-  <img src="../src/images/Mimahome.jpg" className="rounded float-start" id="home-img" alt="..."></img>
+  <img src="../src/images/MimaHome.png" className="rounded float-start" id="home-img" alt="..."></img>
   <div className="card-details">
-<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti laboriosam numquam perspiciatis error accusamus distinctio voluptatem beatae? Eos assumenda voluptatum consectetur quas doloribus temporibus quidem vitae quam tenetur laborum. Quis earum soluta nisi repellat nostrum similique commodi praesentium? Aut, ratione! Ex voluptas aliquid ratione eum et quam, aspernatur excepturi vero.</p>
+    <h2> In Remembrance Of <i> Guadalupe Mendoza </i> </h2>
+    <br/>
+    <h5> September 11th, 1932 - December 6th, 2022 </h5>
+    <br/>
+    <h5> Personal Note </h5>
+<p>We&apos;re blessed to be able to see all of these memories and stories that everyone is sharing and it just goes to show how much of a force of nature Mima truly was.
+</p>
 </div>
 </div>
 
--- Capuano_Krystle_Memorial_Capstone/src/components/Quote.jsx
+++ Capuano_Krystle_Memorial_Capstone/src/components/Quote.jsx
@@ -3,7 +3,7 @@ const Quote = () => {
 
 <figure className="text-center">
   <blockquote className="blockquote" id="quote">
-    <p>A well-known quote, contained in a blockquote element.</p>
+    <p> I will search for you in every sunrise. </p>
   </blockquote>
   <figcaption className="blockquote-footer" id="quote-source">
     Someone famous in <cite title="Source Title">Source Title</cite>
