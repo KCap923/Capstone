5dd14e96d12ff2e46a8df63e5db970dbc20ce6a0
Krystle Capuano <krystlecapuano@gmail.com>
2024-06-24 18:09:15 -0400

    Timeline Columns

--- Capuano_Krystle_Memorial_Capstone/src/components/Timeline.jsx
+++ Capuano_Krystle_Memorial_Capstone/src/components/Timeline.jsx
@@ -1,24 +1,242 @@
-import Navbar from '../components/NavBar'
-import "../components/CSSPages/timeline.css"
-import Footer from "../components/Footer"
-
-export default function Timeline() {
+const TimeTron = () => {
   return (
     <>
-      <Navbar />
       <h1> Timeline </h1>
-      {/* Image for Timeline */}
-      <img src="../src/images/Timeline/Home2.png" className="rounded float-start" id="home-img2" alt="..."></img>
+
+      {/* Timeline Photos */}
+      <img
+        src="../src/images/Timeline/Home2.png"
+        className="rounded float-start"
+        id="home-img3"
+        alt="..."
+      ></img>
+
+<img
+        src="../src/images/Timeline/Home2.png"
+        className="rounded float-start"
+        id="home-img3"
+        alt="..."
+      ></img>
 
       {/* Timeline */}
 
-      
+      <div className="d-flex flex-column flex-md-row p-4 gap-4 py-md-5 align-items-center justify-content-center">
+        <div className="list-group">
+          <a
+            href="#"
+            className="list-group-item list-group-item-action d-flex gap-3 py-3"
+            aria-current="true"
+          >
+            <img
+              src="https://github.com/twbs.png"
+              alt="twbs"
+              width="32"
+              height="32"
+              className="rounded-circle flex-shrink-0"
+            />
+            <div className="d-flex gap-2 w-100 justify-content-between">
+              <div>
+                <h6 className="mb-0">List group item heading</h6>
+                <p className="mb-0 opacity-75">
+                  Some placeholder content in a paragraph.
+                </p>
+              </div>
+              <small className="opacity-50 text-nowrap">now</small>
+            </div>
+          </a>
+          <a
+            href="#"
+            className="list-group-item list-group-item-action d-flex gap-3 py-3"
+            aria-current="true"
+          >
+            <img
+              src="https://github.com/twbs.png"
+              alt="twbs"
+              width="32"
+              height="32"
+              className="rounded-circle flex-shrink-0"
+            />
+            <div className="d-flex gap-2 w-100 justify-content-between">
+              <div>
+                <h6 className="mb-0">Another title here</h6>
+                <p className="mb-0 opacity-75">
+                  Some placeholder content in a paragraph that goes a little
+                  longer so it wraps to a new line.
+                </p>
+              </div>
+              <small className="opacity-50 text-nowrap">3d</small>
+            </div>
+          </a>
+          <a
+            href="#"
+            className="list-group-item list-group-item-action d-flex gap-3 py-3"
+            aria-current="true"
+          >
+            <img
+              src="https://github.com/twbs.png"
+              alt="twbs"
+              width="32"
+              height="32"
+              className="rounded-circle flex-shrink-0"
+            />
+            <div className="d-flex gap-2 w-100 justify-content-between">
+              <div>
+                <h6 className="mb-0">Third heading</h6>
+                <p className="mb-0 opacity-75">
+                  Some placeholder content in a paragraph.
+                </p>
+              </div>
+              <small className="opacity-50 text-nowrap">1w</small>
+            </div>
+          </a>
+        </div>
+      </div>
 
+      <div className="d-flex flex-column flex-md-row p-4 gap-4 py-md-5 align-items-center justify-content-center">
+        <div className="list-group">
+          <a
+            href="#"
+            className="list-group-item list-group-item-action d-flex gap-3 py-3"
+            aria-current="true"
+          >
+            <img
+              src="https://github.com/twbs.png"
+              alt="twbs"
+              width="32"
+              height="32"
+              className="rounded-circle flex-shrink-0"
+            />
+            <div className="d-flex gap-2 w-100 justify-content-between">
+              <div>
+                <h6 className="mb-0">List group item heading</h6>
+                <p className="mb-0 opacity-75">
+                  Some placeholder content in a paragraph.
+                </p>
+              </div>
+              <small className="opacity-50 text-nowrap">now</small>
+            </div>
+          </a>
+          <a
+            href="#"
+            className="list-group-item list-group-item-action d-flex gap-3 py-3"
+            aria-current="true"
+          >
+            <img
+              src="https://github.com/twbs.png"
+              alt="twbs"
+              width="32"
+              height="32"
+              className="rounded-circle flex-shrink-0"
+            />
+            <div className="d-flex gap-2 w-100 justify-content-between">
+              <div>
+                <h6 className="mb-0">Another title here</h6>
+                <p className="mb-0 opacity-75">
+                  Some placeholder content in a paragraph that goes a little
+                  longer so it wraps to a new line.
+                </p>
+              </div>
+              <small className="opacity-50 text-nowrap">3d</small>
+            </div>
+          </a>
+          <a
+            href="#"
+            className="list-group-item list-group-item-action d-flex gap-3 py-3"
+            aria-current="true"
+          >
+            <img
+              src="https://github.com/twbs.png"
+              alt="twbs"
+              width="32"
+              height="32"
+              className="rounded-circle flex-shrink-0"
+            />
+            <div className="d-flex gap-2 w-100 justify-content-between">
+              <div>
+                <h6 className="mb-0">Third heading</h6>
+                <p className="mb-0 opacity-75">
+                  Some placeholder content in a paragraph.
+                </p>
+              </div>
+              <small className="opacity-50 text-nowrap">1w</small>
+            </div>
+          </a>
+        </div>
+      </div>
 
-
-      <Footer/>
+      <div className="d-flex flex-column flex-md-row p-4 gap-4 py-md-5 align-items-center justify-content-center">
+        <div className="list-group">
+          <a
+            href="#"
+            className="list-group-item list-group-item-action d-flex gap-3 py-3"
+            aria-current="true"
+          >
+            <img
+              src="https://github.com/twbs.png"
+              alt="twbs"
+              width="32"
+              height="32"
+              className="rounded-circle flex-shrink-0"
+            />
+            <div className="d-flex gap-2 w-100 justify-content-between">
+              <div>
+                <h6 className="mb-0">List group item heading</h6>
+                <p className="mb-0 opacity-75">
+                  Some placeholder content in a paragraph.
+                </p>
+              </div>
+              <small className="opacity-50 text-nowrap">now</small>
+            </div>
+          </a>
+          <a
+            href="#"
+            className="list-group-item list-group-item-action d-flex gap-3 py-3"
+            aria-current="true"
+          >
+            <img
+              src="https://github.com/twbs.png"
+              alt="twbs"
+              width="32"
+              height="32"
+              className="rounded-circle flex-shrink-0"
+            />
+            <div className="d-flex gap-2 w-100 justify-content-between">
+              <div>
+                <h6 className="mb-0">Another title here</h6>
+                <p className="mb-0 opacity-75">
+                  Some placeholder content in a paragraph that goes a little
+                  longer so it wraps to a new line.
+                </p>
+              </div>
+              <small className="opacity-50 text-nowrap">3d</small>
+            </div>
+          </a>
+          <a
+            href="#"
+            className="list-group-item list-group-item-action d-flex gap-3 py-3"
+            aria-current="true"
+          >
+            <img
+              src="https://github.com/twbs.png"
+              alt="twbs"
+              width="32"
+              height="32"
+              className="rounded-circle flex-shrink-0"
+            />
+            <div className="d-flex gap-2 w-100 justify-content-between">
+              <div>
+                <h6 className="mb-0">Third heading</h6>
+                <p className="mb-0 opacity-75">
+                  Some placeholder content in a paragraph.
+                </p>
+              </div>
+              <small className="opacity-50 text-nowrap">1w</small>
+            </div>
+          </a>
+        </div>
+      </div>
     </>
   );
-}
+};
 
-{/* <img src="../src/images/Timeline/Home3.png" className="rounded float-end" id="home-img3" alt="..."></img> */}
+export default TimeTron;
--- Capuano_Krystle_Memorial_Capstone/src/pages/Timeline.jsx
+++ Capuano_Krystle_Memorial_Capstone/src/pages/Timeline.jsx
@@ -1,10 +1,13 @@
+import TimeTron from "../components/Timeline";
 import Navbar from "../components/NavBar";
 import Footer from "../components/Footer"
+import "../components/CSSPages/timeline.css"
 
 export default function Home() {
   return (
     <>
         <Navbar /> 
+        <TimeTron/>
         <Footer/>
         </>
   );
