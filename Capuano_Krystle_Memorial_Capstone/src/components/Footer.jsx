b6b5a4225dab66493f661ae4b4e9dbfc18942577
Krystle Capuano <krystlecapuano@gmail.com>
2024-06-24 19:00:11 -0400

    Timeline Icons

--- Capuano_Krystle_Memorial_Capstone/src/components/Footer.jsx
+++ Capuano_Krystle_Memorial_Capstone/src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
 
 return (
 
- <footer className="py-3 my-4" id="footer">
+<footer className="py-3 my-4" id="footer">
     <ul className="nav justify-content-center border-bottom pb-3 mb-3">
       <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Home</a></li>
       <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Timeline</a></li>
@@ -10,6 +10,7 @@ return (
       <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Photo Albums</a></li>
     </ul>
     <p className="text-center text-body-secondary">© 2024 Capuano, Inc</p>
+
   </footer> 
 
 )
@@ -17,7 +18,7 @@ return (
 
 export default Footer
 
-
+//Alternative Footer
 {/* <li className="ms-3"><a className="link-body-emphasis" href="#"><svg className="bi" width="24" height="24"><use xlinkHref="#facebook"></use></svg> Share on Facebook </a></li> */}
 
 {/* <div className="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
--- Capuano_Krystle_Memorial_Capstone/src/components/Timeline.jsx
+++ Capuano_Krystle_Memorial_Capstone/src/components/Timeline.jsx
@@ -3,15 +3,8 @@ const TimeTron = () => {
     <>
       <h1> Timeline </h1>
 
-      {/* Timeline Photos */}
+      {/* Timeline Photo */}
       <img
-        src="../src/images/Timeline/Home2.png"
-        className="rounded float-start"
-        id="home-img3"
-        alt="..."
-      ></img>
-
-<img
         src="../src/images/Timeline/Home2.png"
         className="rounded float-start"
         id="home-img3"
@@ -20,220 +13,180 @@ const TimeTron = () => {
 
       {/* Timeline */}
 
-      <div className="d-flex flex-column flex-md-row p-4 gap-4 py-md-5 align-items-center justify-content-center">
+      <div className="d-flex flex-column flex-md-row p-4 gap-4  py-md-5 align-items-center justify-content-center">
         <div className="list-group">
           <a
             href="#"
-            className="list-group-item list-group-item-action d-flex gap-3 py-3"
+            className="list-group-item list-group-item-action d-flex gap-4  py-3"
             aria-current="true"
           >
-            <img
-              src="https://github.com/twbs.png"
-              alt="twbs"
-              width="32"
-              height="32"
-              className="rounded-circle flex-shrink-0"
-            />
-            <div className="d-flex gap-2 w-100 justify-content-between">
+            {/*Cake Icon*/}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              fill="currentColor"
+              className="bi bi-cake"
+              viewBox="0 0 20 20"
+            >
+              <path d="m7.994.013-.595.79a.747.747 0 0 0 .101 1.01V4H5a2 2 0 0 0-2 2v3H2a2 2 0 0 0-2 2v4a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-4a2 2 0 0 0-2-2h-1V6a2 2 0 0 0-2-2H8.5V1.806A.747.747 0 0 0 8.592.802zM4 6a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v.414a.9.9 0 0 1-.646-.268 1.914 1.914 0 0 0-2.708 0 .914.914 0 0 1-1.292 0 1.914 1.914 0 0 0-2.708 0A.9.9 0 0 1 4 6.414zm0 1.414c.49 0 .98-.187 1.354-.56a.914.914 0 0 1 1.292 0c.748.747 1.96.747 2.708 0a.914.914 0 0 1 1.292 0c.374.373.864.56 1.354.56V9H4zM1 11a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v.793l-.354.354a.914.914 0 0 1-1.293 0 1.914 1.914 0 0 0-2.707 0 .914.914 0 0 1-1.292 0 1.914 1.914 0 0 0-2.708 0 .914.914 0 0 1-1.292 0 1.914 1.914 0 0 0-2.708 0 .914.914 0 0 1-1.292 0L1 11.793zm11.646 1.854a1.915 1.915 0 0 0 2.354.279V15H1v-1.867c.737.452 1.715.36 2.354-.28a.914.914 0 0 1 1.292 0c.748.748 1.96.748 2.708 0a.914.914 0 0 1 1.292 0c.748.748 1.96.748 2.707 0a.914.914 0 0 1 1.293 0Z" />
+            </svg>
+            <div className="d-flex gap-4  w-100 justify-content-between">
               <div>
-                <h6 className="mb-0">List group item heading</h6>
-                <p className="mb-0 opacity-75">
-                  Some placeholder content in a paragraph.
-                </p>
+                <h6 className="mb-0">Born</h6>
+                <p className="mb-0 opacity-75">September 11th, 1932</p>
               </div>
-              <small className="opacity-50 text-nowrap">now</small>
+              <small className="opacity-50 text-nowrap">Updated 6/24/24</small>
             </div>
           </a>
+
+          <br />
           <a
             href="#"
-            className="list-group-item list-group-item-action d-flex gap-3 py-3"
+            className="list-group-item list-group-item-action d-flex gap-4  py-3"
             aria-current="true"
           >
-            <img
-              src="https://github.com/twbs.png"
-              alt="twbs"
-              width="32"
-              height="32"
-              className="rounded-circle flex-shrink-0"
-            />
-            <div className="d-flex gap-2 w-100 justify-content-between">
+            {/*Grad Cap Icon*/}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              fill="currentColor"
+              className="bi bi-mortarboard"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917zM8 8.46 1.758 5.965 8 3.052l6.242 2.913z" />
+              <path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466zm-.068 1.873.22-.748 3.496 1.311a.5.5 0 0 0 .352 0l3.496-1.311.22.748L8 12.46z" />
+            </svg>
+            <div className="d-flex gap-4  w-100 justify-content-between">
               <div>
-                <h6 className="mb-0">Another title here</h6>
-                <p className="mb-0 opacity-75">
-                  Some placeholder content in a paragraph that goes a little
-                  longer so it wraps to a new line.
-                </p>
+                <h6 className="mb-0">Graduated From Highschool</h6>
+                <p className="mb-0 opacity-75">1950 From New Mexico High</p>
               </div>
-              <small className="opacity-50 text-nowrap">3d</small>
+              <small className="opacity-50 text-nowrap">Updated 6/24/24</small>
             </div>
           </a>
+
+          <br />
           <a
             href="#"
-            className="list-group-item list-group-item-action d-flex gap-3 py-3"
+            className="list-group-item list-group-item-action d-flex gap-4 py-3"
             aria-current="true"
           >
-            <img
-              src="https://github.com/twbs.png"
-              alt="twbs"
-              width="32"
-              height="32"
-              className="rounded-circle flex-shrink-0"
-            />
-            <div className="d-flex gap-2 w-100 justify-content-between">
+            {/*Person Heart Icon*/}
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              fill="currentColor"
+              className="bi bi-person-heart"
+              viewBox="0 0 16 16"
+            >
+              <path d="M9 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0m-9 8c0 1 1 1 1 1h10s1 0 1-1-1-4-6-4-6 3-6 4m13.5-8.09c1.387-1.425 4.855 1.07 0 4.277-4.854-3.207-1.387-5.702 0-4.276Z" />
+            </svg>
+            <div className="d-flex gap-4  w-100 justify-content-between">
               <div>
-                <h6 className="mb-0">Third heading</h6>
-                <p className="mb-0 opacity-75">
-                  Some placeholder content in a paragraph.
-                </p>
+                <h6 className="mb-0">Met Honey</h6>
+                <p className="mb-0 opacity-75">While Attending College</p>
               </div>
-              <small className="opacity-50 text-nowrap">1w</small>
+              <small className="opacity-50 text-nowrap">Updated 6/20/24</small>
+            </div>
+          </a>
+    
+
+      <div className="d-flex flex-column flex-md-row p-1 gap-4  py-md-5 align-items-center justify-content-center">
+        <div className="list-group">
+          <a
+            href="#"
+            className="list-group-item list-group-item-action d-flex gap-4  py-3"
+            aria-current="true"
+          >
+            {/*Heart Icon*/}
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-suit-heart-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1" />
+            </svg>
+            <div className="d-flex gap-4 w-100 justify-content-between">
+              <div>
+                <h6 className="mb-0">Got Married</h6>
+                <p className="mb-0 opacity-75">June 28th, 1935</p>
+              </div>
+              <small className="opacity-50 text-nowrap">Updated 6/22/24</small>
+            </div>
+          </a>
+
+          <br />
+          <a
+            href="#"
+            className="list-group-item list-group-item-action d-flex gap-4  py-3"
+            aria-current="true"
+          >
+            {/*House Icon*/}
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              fill="currentColor"
+              className="bi bi-house-heart-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M7.293 1.5a1 1 0 0 1 1.414 0L11 3.793V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v3.293l2.354 2.353a.5.5 0 0 1-.708.707L8 2.207 1.354 8.853a.5.5 0 1 1-.708-.707z" />
+              <path d="m14 9.293-6-6-6 6V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5zm-6-.811c1.664-1.673 5.825 1.254 0 5.018-5.825-3.764-1.664-6.691 0-5.018" />
+            </svg>
+            <div className="d-flex gap-4  w-100 justify-content-between">
+              <div>
+                <h6 className="mb-0">Moved To Tucson & Purchased First Home</h6>
+                <p className="mb-0 opacity-75">Tucson, Az</p>
+              </div>
+              <small className="opacity-50 text-nowrap">Updated 6/22/24</small>
+            </div>
+          </a>
+
+          <br />
+          <a
+            href="#"
+            className="list-group-item list-group-item-action d-flex gap-4  py-3"
+            aria-current="true"
+          >
+            {/*Plane Icon*/}
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              fill="currentColor"
+              className="bi bi-airplane-engines"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 0c-.787 0-1.292.592-1.572 1.151A4.35 4.35 0 0 0 6 3v3.691l-2 1V7.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.191l-1.17.585A1.5 1.5 0 0 0 0 10.618V12a.5.5 0 0 0 .582.493l1.631-.272.313.937a.5.5 0 0 0 .948 0l.405-1.214 2.21-.369.375 2.253-1.318 1.318A.5.5 0 0 0 5.5 16h5a.5.5 0 0 0 .354-.854l-1.318-1.318.375-2.253 2.21.369.405 1.214a.5.5 0 0 0 .948 0l.313-.937 1.63.272A.5.5 0 0 0 16 12v-1.382a1.5 1.5 0 0 0-.83-1.342L14 8.691V7.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v.191l-2-1V3c0-.568-.14-1.271-.428-1.849C9.292.591 8.787 0 8 0M7 3c0-.432.11-.979.322-1.401C7.542 1.159 7.787 1 8 1s.458.158.678.599C8.889 2.02 9 2.569 9 3v4a.5.5 0 0 0 .276.447l5.448 2.724a.5.5 0 0 1 .276.447v.792l-5.418-.903a.5.5 0 0 0-.575.41l-.5 3a.5.5 0 0 0 .14.437l.646.646H6.707l.647-.646a.5.5 0 0 0 .14-.436l-.5-3a.5.5 0 0 0-.576-.411L1 11.41v-.792a.5.5 0 0 1 .276-.447l5.448-2.724A.5.5 0 0 0 7 7z" />
+            </svg>
+            <div className="d-flex gap-4  w-100 justify-content-between">
+              <div>
+                <h6 className="mb-0">Traveled Around The World</h6>
+                <p className="mb-0 opacity-75">From 1960-1965</p>
+              </div>
+              <small className="opacity-50 text-nowrap">
+                {" "}
+                Updated 6/24/24{" "}
+              </small>
             </div>
           </a>
         </div>
       </div>
 
-      <div className="d-flex flex-column flex-md-row p-4 gap-4 py-md-5 align-items-center justify-content-center">
-        <div className="list-group">
-          <a
-            href="#"
-            className="list-group-item list-group-item-action d-flex gap-3 py-3"
-            aria-current="true"
-          >
-            <img
-              src="https://github.com/twbs.png"
-              alt="twbs"
-              width="32"
-              height="32"
-              className="rounded-circle flex-shrink-0"
-            />
-            <div className="d-flex gap-2 w-100 justify-content-between">
-              <div>
-                <h6 className="mb-0">List group item heading</h6>
-                <p className="mb-0 opacity-75">
-                  Some placeholder content in a paragraph.
-                </p>
-              </div>
-              <small className="opacity-50 text-nowrap">now</small>
-            </div>
-          </a>
-          <a
-            href="#"
-            className="list-group-item list-group-item-action d-flex gap-3 py-3"
-            aria-current="true"
-          >
-            <img
-              src="https://github.com/twbs.png"
-              alt="twbs"
-              width="32"
-              height="32"
-              className="rounded-circle flex-shrink-0"
-            />
-            <div className="d-flex gap-2 w-100 justify-content-between">
-              <div>
-                <h6 className="mb-0">Another title here</h6>
-                <p className="mb-0 opacity-75">
-                  Some placeholder content in a paragraph that goes a little
-                  longer so it wraps to a new line.
-                </p>
-              </div>
-              <small className="opacity-50 text-nowrap">3d</small>
-            </div>
-          </a>
-          <a
-            href="#"
-            className="list-group-item list-group-item-action d-flex gap-3 py-3"
-            aria-current="true"
-          >
-            <img
-              src="https://github.com/twbs.png"
-              alt="twbs"
-              width="32"
-              height="32"
-              className="rounded-circle flex-shrink-0"
-            />
-            <div className="d-flex gap-2 w-100 justify-content-between">
-              <div>
-                <h6 className="mb-0">Third heading</h6>
-                <p className="mb-0 opacity-75">
-                  Some placeholder content in a paragraph.
-                </p>
-              </div>
-              <small className="opacity-50 text-nowrap">1w</small>
-            </div>
-          </a>
-        </div>
+      <div className="d-flex flex-column flex-md-row p-6 gap-4  py-md-5 align-items-center justify-content-center">
+        <div className="list-group"></div>
       </div>
-
-      <div className="d-flex flex-column flex-md-row p-4 gap-4 py-md-5 align-items-center justify-content-center">
-        <div className="list-group">
-          <a
-            href="#"
-            className="list-group-item list-group-item-action d-flex gap-3 py-3"
-            aria-current="true"
-          >
-            <img
-              src="https://github.com/twbs.png"
-              alt="twbs"
-              width="32"
-              height="32"
-              className="rounded-circle flex-shrink-0"
-            />
-            <div className="d-flex gap-2 w-100 justify-content-between">
-              <div>
-                <h6 className="mb-0">List group item heading</h6>
-                <p className="mb-0 opacity-75">
-                  Some placeholder content in a paragraph.
-                </p>
-              </div>
-              <small className="opacity-50 text-nowrap">now</small>
-            </div>
-          </a>
-          <a
-            href="#"
-            className="list-group-item list-group-item-action d-flex gap-3 py-3"
-            aria-current="true"
-          >
-            <img
-              src="https://github.com/twbs.png"
-              alt="twbs"
-              width="32"
-              height="32"
-              className="rounded-circle flex-shrink-0"
-            />
-            <div className="d-flex gap-2 w-100 justify-content-between">
-              <div>
-                <h6 className="mb-0">Another title here</h6>
-                <p className="mb-0 opacity-75">
-                  Some placeholder content in a paragraph that goes a little
-                  longer so it wraps to a new line.
-                </p>
-              </div>
-              <small className="opacity-50 text-nowrap">3d</small>
-            </div>
-          </a>
-          <a
-            href="#"
-            className="list-group-item list-group-item-action d-flex gap-3 py-3"
-            aria-current="true"
-          >
-            <img
-              src="https://github.com/twbs.png"
-              alt="twbs"
-              width="32"
-              height="32"
-              className="rounded-circle flex-shrink-0"
-            />
-            <div className="d-flex gap-2 w-100 justify-content-between">
-              <div>
-                <h6 className="mb-0">Third heading</h6>
-                <p className="mb-0 opacity-75">
-                  Some placeholder content in a paragraph.
-                </p>
-              </div>
-              <small className="opacity-50 text-nowrap">1w</small>
-            </div>
-          </a>
-        </div>
+      </div>
       </div>
     </>
   );
