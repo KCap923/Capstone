f80eef65ebd35d65a5333e890f71bf21faa5263e
Krystle Capuano <krystlecapuano@gmail.com>
2024-06-25 16:34:33 -0400

    Video Edit

--- Capuano_Krystle_Memorial_Capstone/src/components/Comments.jsx
+++ Capuano_Krystle_Memorial_Capstone/src/components/Comments.jsx
@@ -1,6 +1,6 @@
 import { useRef } from "react";
 
-export default function Comments () {
+export default function Comments ({comments, setComments, baseUrl}) {
 
   // UseRef keeps a reference to each of the elements below to see current value that is typed into them by user
 const nameRef=useRef(null)
@@ -8,9 +8,25 @@ const relRef=useRef(null)
 const commRef=useRef(null)
 const dateRef=useRef(null)
 
+// Set handle submit, what happens when form is submitted. prevent.Default is set so page doesnt automatically refresh
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+try {
+  console.log(nameRef.current.value)
+  console.log(nameRef.current.value)
+  console.log(nameRef.current.value)
+  console.log(nameRef.current.value)
+
+} catch (err) {
+  console.log(err)
+}
+};
+
+
 
 return (
- <form className="NewComment">
+ <form onSubmit={handleSubmit} className="NewComment">
 Name:<input type="text" ref={nameRef}/>
 <br />
 Relationship:<input type="text" ref={relRef}/>
--- Capuano_Krystle_Memorial_Capstone/src/pages/Memory.jsx
+++ Capuano_Krystle_Memorial_Capstone/src/pages/Memory.jsx
@@ -1,23 +1,32 @@
-import { useState } from 'react'
-import Navbar from '../components/NavBar'
-import MemoryTron from "../components/Memory"
-import NewComment from '../components/Comments'
-import "../components/CSSPages/memory.css"
-import Footer from "../components/Footer"
-
+import { useState } from "react";
+import Navbar from "../components/NavBar";
+import MemoryTron from "../components/Memory";
+import NewComment from "../components/Comments";
+import "../components/CSSPages/memory.css";
+import Footer from "../components/Footer";
 
 export default function Memory() {
+  //Holds all of the State and fetches all the comments to display
+  const [Comments, setComments] = useState([]);
 
-  const [Comments, setComments] = useState ([])
+  // Grabbing API/ Send a request to our API when someone submits a comment
+  const baseUrl = "https://capstone-memorial-backend.onrender.com";
 
   return (
     <>
       <Navbar />
       <h1> Memory Page </h1>
+        <MemoryTron />
+        <br />
       {/* Creating prop (comments={Comments}) and passing setter function (setComments={setComments}) */}
-      <NewComment comments={Comments} setComments={setComments}/>
-      <MemoryTron/>
-      <Footer/>
+      {/* Once we get a new comment after getting a response, then updates  */}
+      <NewComment
+        comments={Comments}
+        setComments={setComments}
+        baseURL={baseUrl}
+      />
+    
+      <Footer />
     </>
   );
 }
