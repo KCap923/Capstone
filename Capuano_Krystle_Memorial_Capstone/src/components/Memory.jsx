3f1cd92457574aa9ad3691d5c4cb47db088577c8
Krystle Capuano <krystlecapuano@gmail.com>
2024-06-24 20:16:51 -0400

    Memory Youtube Video

--- Capuano_Krystle_Memorial_Capstone/src/components/Memory.jsx
+++ Capuano_Krystle_Memorial_Capstone/src/components/Memory.jsx
@@ -2,8 +2,11 @@ const MemoryTron = () => {
   return (
 <>
 
+<p> I look for you in all of the things you loved, that is where I find you. </p>
 
-
+<div className="video">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/Lo-7djg2aPE?si=HBKSR8Us2ScRMeGv" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</div>
 
 
 </>
